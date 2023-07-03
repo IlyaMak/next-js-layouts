e95021527a4ada1e5b2d111836e23639bc6dee39
Ilia Makarski <ilmobdev@gmail.com>
2023-07-03 13:40:01 +0200

    Add route to sign-in-nft layout

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <a href="sign-in-nft">Sign-in-NFT</a>
+    </div>
+  )
+}
---
+++ src/app/sign-in-nft/page.jsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+    return (
+        <div>
+            Sign-in
+        </div>
+    )
+}
+
+export default SignIn
