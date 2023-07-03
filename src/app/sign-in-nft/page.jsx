ea53f345f4bbdf1be141a3d86b21e47c174aa75c
Ilia Makarski <ilmobdev@gmail.com>
2023-07-03 19:46:26 +0200

    Add sign-in form with inputs

--- src/app/sign-in-nft/page.jsx
+++ src/app/sign-in-nft/page.jsx
@@ -1,7 +1,42 @@
+import styles from '../sign-in-nft/page.module.css'
+
 const SignIn = () => {
     return (
-        <div>
-            Sign-in
+        <div className={styles.main}>
+            <div className={styles.logoContainer}>
+                <img src="/assets-sign-in-nft/Frame.svg" alt="PNFT Market" />
+                <div className={styles.logoText}>PNFT Market</div>
+            </div>
+            <div className={styles.signInContainer}>
+                <div className={styles.signInForm}>
+                    <div className={styles.headlineContainer}>
+                        <div className={styles.headlineText}>
+                            NFT Access
+                        </div>
+                        <div className={styles.hintText}>
+                            Please fill your detail to access your account.
+                        </div>
+                    </div>
+                    <div className={styles.emailContainer}>
+                        <div className={styles.inputLabel}>
+                            Email
+                        </div>
+                        <div className={styles.inputContent}>
+                            <input type="text" className={styles.input} />
+                            <img className={styles.inputImage} src="/assets-sign-in-nft/X Icon.svg" alt="Email input" />
+                        </div>
+                    </div>
+                    <div className={styles.passwordContainer}>
+                        <div className={styles.inputLabel}>
+                            Password
+                        </div>
+                        <div className={styles.inputContent}>
+                            <input type="password" className={styles.input} />
+                            <img className={styles.inputImage} src="/assets-sign-in-nft/Hide Icon.svg" alt="Email input" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
