34d79e2cb5842d5c8f886744dac202b5f8c67df7
Ilia Makarski <ilmobdev@gmail.com>
2023-07-06 12:50:16 +0200

    Add image assets to digital-agency-enver landing

---
+++ src/app/digital-agency-enver/page.jsx
@@ -0,0 +1,15 @@
+import styles from '../digital-agency-enver/page.module.css'
+
+export const metadata = {
+    title: 'Digital Agency Enver'
+}
+
+const Enver = () => {
+    return (
+        <div className={styles.main}>
+
+        </div>
+    )
+}
+
+export default Enver
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.main}>
       <a href="sign-in-nft">Sign-in-NFT</a>
       <a href="digital-agency-enver">Digital Agency Enver</a>
     </div>
