ce05015038a5aa0a601f1c87b1e917d86a721b96
Ilia Makarski <ilmobdev@gmail.com>
2023-07-06 13:56:43 +0200

    Add logo section

--- src/app/digital-agency-enver/page.jsx
+++ src/app/digital-agency-enver/page.jsx
@@ -7,7 +7,10 @@ export const metadata = {
 const Enver = () => {
     return (
         <div className={styles.main}>
-
+            <div className={styles["logo-section"]}>
+                <img className={styles.logo} src="/assets-digital-agency-enver/images/logo.svg" alt="Enver logo" />
+                <img className={styles["menu-logo"]} src="/assets-digital-agency-enver/images/Sort.svg" alt="Menu logo" />
+            </div>
         </div>
     )
 }
