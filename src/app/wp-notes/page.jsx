99f21c9632afb5d891341b5ded6e78bcfcd751b6
Ilia Makarski <ilmobdev@gmail.com>
2023-07-12 15:59:11 +0200

    Add new landing

---
+++ src/app/wp-notes/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export const metadata = {
+  title: "WP Notes",
+};
+
+const Notes = () => {
+  return <div className={styles.main}></div>;
+};
+
+export default Notes;
