4737a1ff24259171425188ffde9e3ae14b4d8914
Ilia Makarski <ilmobdev@gmail.com>
2023-07-18 00:10:19 +0200

    Add all-short-articles-list component

---
+++ src/components/wp-notes/all-short-articles-list/all-short-articles-list.jsx
@@ -0,0 +1,50 @@
+import styles from "./all-short-articles-list.module.css";
+import cnCommon from "../../../modules/classname";
+import ShortArticle from "../short-article/short-article";
+
+const cn = cnCommon.bind(null, styles);
+
+export default function AllShortArticlesList({ otherArticles }) {
+  return (
+    <div className={cn("all-articles-section")}>
+      <div className={cn("header-additional-info", "row")}>PINNED</div>
+      <div className={cn("article", "article--pinned")}>
+        <div className={cn("article__header", "row")}>
+          Light & Bright in Brooklyn
+        </div>
+        <div
+          className={cn(
+            "article__description",
+            "article__content-text",
+            "row"
+          )}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu
+          dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at
+          arcu dui. consectetur adipisce placerat Lorem ipsum dolor sit amet:
+        </div>
+        <div
+          className={cn("article__datetime", "article__content-text", "row")}>
+          Feb 8, 2021
+        </div>
+      </div>
+      <div className={styles["other-header-section"]}>
+        <div className={styles["horizontal-line"]}></div>
+        <div
+          className={cn(
+            "header-additional-info",
+            "header-additional-info--other",
+            "row"
+          )}>
+          OTHER
+        </div>
+        <div className={styles["horizontal-line"]}></div>
+      </div>
+      {otherArticles.map((shortArticle, shortArticleIndex) => (
+        <ShortArticle key={shortArticleIndex} shortArticle={shortArticle} />
+      ))}
+      <div className={cn("button-all-posts-container")}>
+        <button className={cn("button-all-posts")}>All Posts</button>
+      </div>
+    </div>
+  );
+}
