8f3d7a49dd5cb309ee61f8d5d1028d210d908cda
Ilia Makarski <ilmobdev@gmail.com>
2023-07-18 00:10:43 +0200

    Add other-articles constant

---
+++ src/constants/wp-notes/other-articles.js
@@ -0,0 +1,30 @@
+export const otherArticles = [
+  {
+    header: "Business Partners Work at Modern Office",
+    tags: [],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu dui. consectetur adipisce placerat Lorem ipsum dolor sit amet:",
+    datetime: "Feb 8, 2021",
+  },
+  {
+    header: "Light & Bright in Brooklyn",
+    tags: [],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu dui. consectetur adipisce placerat Lorem ipsum dolor sit amet:",
+    datetime: "Feb 8, 2021",
+  },
+  {
+    header: "Light & Bright in Brooklyn",
+    tags: [],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu dui. consectetur adipisce placerat Lorem ipsum dolor sit amet:",
+    datetime: "Feb 8, 2021",
+  },
+  {
+    header: "Business Partners Work at Modern Office",
+    tags: ["office", "remote"],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at arcu dui. consectetur adipisce placerat Lorem ipsum dolor sit amet:",
+    datetime: "Feb 8, 2021",
+  },
+];
