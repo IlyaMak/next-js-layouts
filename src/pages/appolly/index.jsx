82d294f8172bcdd921b2262de5c49c46047ab581
Ilia Makarski <ilmobdev@gmail.com>
2023-07-23 18:20:54 +0200

    Add header section

--- src/pages/appolly/index.jsx
+++ src/pages/appolly/index.jsx
@@ -1,4 +1,9 @@
 import Head from "next/head";
+import styles from "./page.module.css";
+import cnCommon from "../../modules/classname";
+import globalStyles from "../../app/style.global";
+
+const cn = cnCommon.bind(null, styles);
 
 const Appolly = () => {
   return (
@@ -6,6 +11,84 @@ const Appolly = () => {
       <Head>
         <title>Appolly</title>
       </Head>
+      <style jsx global>
+        {globalStyles}
+      </style>
+      <div className={cn("main")}>
+        <div className={cn("logo-container")}>
+          <img
+            className={cn("logo")}
+            src="/assets-appolly/images/Logo4 1.svg"
+            alt="Appolly logo"
+          />
+        </div>
+        <div className={cn("header")}>
+          <div className={cn("contact-section")}>
+            <div className={cn("contact-container")}>
+              <div className={cn("email-container")}>
+                <img
+                  className={cn("contact-image")}
+                  src="/assets-appolly/images/mail-fill.svg"
+                  alt="Email icon"
+                />
+                <div className={cn("contact-text")}>info@youremail.com</div>
+              </div>
+              <div className={cn("phone-container")}>
+                <img
+                  className={cn("contact-image")}
+                  src="/assets-appolly/images/phone-fill.svg"
+                  alt="Phone icon"
+                />
+                <div className={cn("contact-text")}>(480) 555-0103</div>
+              </div>
+            </div>
+            <div className={cn("social-container")}>
+              <img
+                className={cn("social-image")}
+                src="/assets-appolly/images/facebook-fill.svg"
+                alt="Facebook icon"
+              />
+              <img
+                className={cn("social-image")}
+                src="/assets-appolly/images/instagram-fill.svg"
+                alt="Instagram icon"
+              />
+              <img
+                className={cn("social-image")}
+                src="/assets-appolly/images/twitter-fill.svg"
+                alt="Twitter icon"
+              />
+              <img
+                className={cn("social-image")}
+                src="/assets-appolly/images/youtube-fill.svg"
+                alt="Youtube icon"
+              />
+            </div>
+          </div>
+          <div className={cn("menu-container")}>
+            <button className={cn("menu-button")}>
+              <img src="/assets-appolly/images/menu-line.svg" alt="Menu icon" />
+            </button>
+            <div className={cn("main-menu-items-container")}>
+              <div className={cn("menu-item")}>HOME</div>
+              <div className={cn("menu-item")}>ABOUT</div>
+              <div className={cn("menu-item")}>FEATURES</div>
+            </div>
+            <div className={cn("secondary-menu-items-container")}>
+              <div className={cn("menu-item")}>SCREENSHOT</div>
+              <div className={cn("menu-item")}>BLOG</div>
+            </div>
+            <button className={cn("download-button")}>DOWNLOAD</button>
+          </div>
+        </div>
+        <div className={cn("home-section")}>
+          <div className={cn("download-container")}></div>
+          <img
+            src="/assets-appolly/images/Free Ui View Mobile App Mockup 1.png"
+            alt="Appolly image"
+          />
+        </div>
+      </div>
     </>
   );
 };
